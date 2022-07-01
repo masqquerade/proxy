317ac60674002fbd9c0e9b386e3323e1942f63b0
msqrd <davakozu@gmail.com>
2022-07-01 13:55:58 +0200

    proxy fun

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -1,9 +1,40 @@
 package proxy
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
 
 type Proxy struct{}
 
 func (p *Proxy) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	sUrl, err := url.Parse("http://localhost:8081")
+	if err != nil {
+		fmt.Fprint(rw, err)
+	}
 
+	r.Host = sUrl.Host
+	r.URL.Host = sUrl.Host
+	r.URL.Scheme = sUrl.Scheme
+	r.RequestURI = ""
+
+	client := &http.Client{}
+
+	res, err := client.Do(r)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(rw, err)
+		return
+	}
+
+	for key, values := range res.Header {
+		for _, value := range values {
+			rw.Header().Set(key, value)
+		}
+	}
+
+	rw.WriteHeader(res.StatusCode)
+	io.Copy(rw, res.Body)
 }
